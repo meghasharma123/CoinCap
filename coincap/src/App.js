27e0d6326713f923ea2b51ab7f583d13eb97eceb
meghasharma123 <meghasharma282200@gmail.com>
2022-11-30 19:05:34 +0530

    update

--- coincap/src/App.js
+++ coincap/src/App.js
@@ -1,97 +1,120 @@
 import './App.css';
 import './Component/Table'
-import React,{useMemo,useState,useEffect} from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import Table from './Component/Table';
 
 function App() {
-  const [data,setData] = useState([]);
-  const [tableVal,setTableVal ] = useState(20)
+  const [data, setData] = useState([]);
+  const [tableVal, setTableVal] = useState(20)
+  const [options, setOptions] = useState([]);
+  const [faits, setFaits] = useState('INR');
   const columns = useMemo(
     () => [
-          {
-            Header: "Rank",
-            accessor: "rank"
-          },
-          {
-            Header: "Price",
-            accessor: "price",
-            Cell: ({ cell: { value } }) => {
-              const marC = (value / 1000).toFixed(5);
-              return (
-                <>
-                  {`${marC}k`}
-                </>
-              );
-            }
-          },
-          {
-            Header: "Market Cap",
-            accessor: "marketCap",
-            Cell: ({ cell: { value } }) => {
-              const marC = (value / 1000000).toFixed(2);
-              return (
-                <>
-                  {marC > 0 ? `${marC}m`: ""}
-                </>
-              );
-            }
-          },
-          {
-            Header: "Volume",
-            accessor: "volume",
-            Cell: ({ cell: { value } }) => {
-              const vol = (value / 1000).toFixed(2);
-              return (
-                <>
-                  {vol > 0 ? `${vol}k`: ""}
-                </>
-              );
-            }
-          },
-          {
-            Header: "Supply",
-            accessor: "totalSupply",
-            Cell: ({ cell: { value } }) => {
-              const vol = (value / 1000).toFixed(2);
-              return (
-                <>
-                  {vol > 0 ? `${vol}k`: ""}
-                </>
-              );
-            }
-          },
-          {
-            Header: "Price Change(1h)",
-            accessor: "priceChange1h"
-          },
-          {
-            Header: "Price Change(1d)",
-            accessor: "priceChange1d"
-          },
-          {
-            Header: "Price Change(1w)",
-            accessor: "priceChange1w"
-          }
+      {
+        Header: "Rank",
+        accessor: "rank"
+      },
+      {
+        Header: "Name",
+        accessor: "name"
+      },
+      {
+        Header: "Price",
+        accessor: "price",
+        Cell: ({ cell: { value } }) => {
+          const marC = (value / 1000).toFixed(5);
+          return (
+            <>
+              {`${marC}k`}
+            </>
+          );
+        }
+      },
+      {
+        Header: "Market Cap",
+        accessor: "marketCap",
+        Cell: ({ cell: { value } }) => {
+          const marC = (value / 1000000).toFixed(2);
+          return (
+            <>
+              {marC > 0 ? `${marC}m` : ""}
+            </>
+          );
+        }
+      },
+      {
+        Header: "Volume",
+        accessor: "volume",
+        Cell: ({ cell: { value } }) => {
+          const vol = (value / 1000).toFixed(2);
+          return (
+            <>
+              {vol > 0 ? `${vol}k` : ""}
+            </>
+          );
+        }
+      },
+      {
+        Header: "Supply",
+        accessor: "totalSupply",
+        Cell: ({ cell: { value } }) => {
+          const vol = (value / 1000).toFixed(2);
+          return (
+            <>
+              {vol > 0 ? `${vol}k` : ""}
+            </>
+          );
+        }
+      },
+      {
+        Header: "Price Change(1h)",
+        accessor: "priceChange1h"
+      },
+      {
+        Header: "Price Change(1d)",
+        accessor: "priceChange1d"
+      },
+      {
+        Header: "Price Change(1w)",
+        accessor: "priceChange1w"
+      }
     ],
     []
   );
-  const handleViewMore = ()=>{
-    setTableVal(prev=>prev+20);
+
+  const handleViewMore = () => {
+    setTableVal(prev => prev + 20);
   }
-  useEffect(()=>{
-    fetch('https://api.coinstats.app/public/v1/coins?skip=0&limit='+tableVal+'&currency=EUR').then((ele)=>ele.json()).then((ele)=>setData(ele.coins))
-  },[tableVal])
-  console.log(data)
+
+  const handleOption = (e) => {
+    setFaits(e.target.value)
+  }
+
+  useEffect(() => {
+    fetch('https://api.coinstats.app/public/v1/coins?skip=0&limit=' + tableVal + '&currency=' + faits).then((ele) => ele.json()).then((ele) => setData(ele.coins))
+  }, [tableVal, faits])
+
+  useEffect(() => {
+    fetch('https://api.coinstats.app/public/v1/fiats').then((ele) => ele.json()).then((ele) => setOptions(ele));
+  }, [])
+
   return (
     <div className="App">
-    <div className='table'>
-      {
-        data.length>0?<Table data={data} columns={columns}/>:''
-      }
-    </div>
-    <div className='viewMore'>
-      <button onClick={handleViewMore}>View More</button>
-    </div>
+      <div className='dropDown'>
+        <select className='selectOption' onChange={handleOption} placeholder="INR" value={faits}>
+          {options.map((option) => (
+            <option key={option.name} value={option.name}>{option.name} Hii</option>
+          ))}
+        </select>
+      </div>
+      <div className='table'>
+        {
+          data.length > 0 ? <Table data={data} columns={columns} /> : ''
+        }
+      </div>
+      <div className='viewMore'>
+        <button onClick={handleViewMore}>View More</button>
+      </div>
     </div>
   );
 }
--- coincap/src/Component/Table.js
+++ coincap/src/Component/Table.js
@@ -1,46 +1,45 @@
 import React from "react";
-import { useTable,useSortBy } from "react-table";
+import { useTable, useSortBy } from "react-table";
 import './table.css'
 
-const Table = ({data,columns}) =>{
+const Table = ({ data, columns }) => {
     const {
         getTableProps, // table props from react-table
         getTableBodyProps, // table body props from react-table
         headerGroups, // headerGroups, if your table has groupings
         rows, // rows for the table based on the data passed
         prepareRow // Prepare the row (this function needs to be called for each row before getting the row props)
-      } = useTable(
+    } = useTable(
         {
-          columns,
-          data
+            columns,
+            data
         },
         useSortBy // This plugin Hook will help to sort our table columns
-      );
-      console.log("table",data)
-    return(
+    );
+    return (
         <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())} className={column.isSorted? column.isSortedDesc? "desc": "asc": ""}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())} className={column.isSorted ? column.isSortedDesc ? "desc" : "asc" : ""}>{column.render("Header")}</th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
     );
 }
 export default Table;
